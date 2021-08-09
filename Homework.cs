54d549b4cc5269ef90f30f301d293bead6cd7c8b
HaykAzaryan <88545353+HaykAzaryan@users.noreply.github.com>
2021-08-09 17:04:54 +0400

    Add files via upload

---
+++ Homework.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void AnotherEnrty()
+        {
+            Console.WriteLine("Hello World Through Another Entry");
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
